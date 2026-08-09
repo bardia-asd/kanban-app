4a66677cc9159360f514dc43f410a18a7fc2a3e1
bardia-asd <brdyaasdy5@gmail.com>
2026-08-09 19:34:53 +0330

    feat: add design tokens for colors, typography, radius, and shadows

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 const App = () => {
-    return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <>
+            <h1 class="text-3xl font-bold underline">Hello world!</h1>
+            <div className="size-10 bg-primary"></div>;
+        </>
+    );
 };
 
 export default App;
